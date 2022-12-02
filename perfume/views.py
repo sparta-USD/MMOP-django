754ee8fae56f695b73ec9c9326d8c2b421b70314
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-02 14:07:04 +0900

    Feat : 리뷰 CRUD 완성 #3
    - User, Perfume ForeignKey 설정 나중에 수정해야함!
    - views.py에서 post 메소드 : serializer.save() 코드 추가
    
    Close #3

--- perfume/views.py
+++ perfume/views.py
@@ -20,7 +20,7 @@ class ReviewView(APIView):
     def post(self, request):
         serializer = ReviewCreateSerializer(data=request.data)
         if serializer.is_valid():
-            # serializer.save(user=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
