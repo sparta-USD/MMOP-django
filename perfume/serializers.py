254b5ed6c93ac60b92cf44788d4966ad3b305c39
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-02 11:47:38 +0900

    Feat : ReviewSerializer 생성 #3
    - perfume/serializers.py 생성했음.
    - ReviewSerializer, ReviewCreateSerializer 추가했음.

---
+++ perfume/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from perfume.models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ("good_content", "bad_content", "grade", "image")
