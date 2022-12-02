6adf17f73b039efa66bdff5dd88b41c3dcf400b2
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-02 11:55:33 +0900

    Feat : 리뷰조회, 리뷰작성 API 구현
    - 리뷰를 조회하고, 리뷰를 작성하는 API 구현했음.
    - 'reviews/' url 입력 시 작동

--- perfume/views.py
+++ perfume/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from perfume import serializers
+from perfume.models import Review
+from perfume.serializers import ReviewSerializer,ReviewCreateSerializer
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+class ReviewView(APIView):
+    # 리뷰 목록 조회하기
+    def get(self, request): # perfume_id 추가 필요!
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    # 리뷰 작성하기
+    def post(self, request):
+        serializer = ReviewCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            # serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
