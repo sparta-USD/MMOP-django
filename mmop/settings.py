b55eef0d70851f96c0662cf5d392fccbd64c9a46
haeminchoi2 <sjhk02@gmail.com>
2022-12-02 11:37:28 +0900

    Feat: 기본 유저 모델 settings.py 추가 #2
    - Django settings.py에 기본 유저모델 추가했습니다.

--- mmop/settings.py
+++ mmop/settings.py
@@ -135,6 +135,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=720),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
