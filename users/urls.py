f38616a8819e5ef3b97245c394776b801eeb64ca
suin <parksuin55@gmail.com>
2022-12-02 20:04:21 +0900

    Feat: 프로필 수정 기능 추가
    1. 프로필 get 작업
    	- perfume, review, custom_perfume은 추후 추가 예정
    2. 프로필 수정 추가

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('signin/', views.SigninView.as_view(), name='signin'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('<str:username>/', views.ProfileView.as_view(), name='profile'),
 ]
