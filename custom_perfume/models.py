a20a0efc895c6b15906575ca3ca2c3638a831886
dy <kvkvd12@gmail.com>
2022-12-02 14:13:30 +0900

    Feat: 향 모델 생성 #5
    - 향 모델 생성
    - 향 이미지 난수로 생성 및 저장위치 설정
    - 주석처리한 외래키 모델생성으로 주석해제
    - 아직 생성되지 않은 모델로 인해 카테고리 주석처리
    - 실수로 가져온 기성향수 모델 삭제

--- custom_perfume/models.py
+++ custom_perfume/models.py
@@ -11,17 +11,29 @@ def upload_to_custom_perfume_logo(instance, filename):
     filename = '{}.{}'.format(uuid, ext)
     return os.path.join(upload_to, filename)
 
+def upload_to_note_image(instance, filename):
+    upload_to = f'note_image/'
+    ext = filename.split('.')[-1]
+    uuid = uuid4().hex
+    filename = '{}.{}'.format(uuid, ext)
+    return os.path.join(upload_to, filename)
 
-class Perfume(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=100)
-
+    type = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=upload_to_note_image, max_length=255)
+    # note_category = models.ForeignKey(모델, on_delete=models.CASCADE)
+    
 class CustomPerfume(models.Model):
     title = models.CharField(max_length=100)
     logo = models.ImageField(upload_to=upload_to_custom_perfume_logo, max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     # creator = models.ForeignKey(모델, on_delete=models.CASCADE)
-    # note01 = models.ForeignKey(모델, on_delete=models.CASCADE)
-    # note02 = models.ForeignKey(모델, on_delete=models.CASCADE)
-    # note03 = models.ForeignKey(모델, on_delete=models.CASCADE)
+    note01 = models.ForeignKey(Note, on_delete=models.CASCADE)
+    note02 = models.ForeignKey(Note, on_delete=models.CASCADE)
+    note03 = models.ForeignKey(Note, on_delete=models.CASCADE)
     # package = models.ForeignKey(모델, on_delete=models.CASCADE)
+    
+
+    
 
