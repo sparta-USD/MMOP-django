9004fa7f9eb0f619f5dcc23ac428a19fbabe78d4
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-02 11:35:49 +0900

    Feat: Review 모델 생성 #3
    - Review 모델 생성

--- perfume/models.py
+++ perfume/models.py
@@ -5,3 +5,15 @@ class Perfume(models.Model):
     title = models.CharField(max_length=100)
 
     
+class Review(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_reviews")
+    # perfume = models.ForeignKey(Perfume, on_delete=models.CASCADE, related_name="perfume_reviews")
+    good_content = models.TextField()
+    bad_content = models.TextField()
+    grade = models.IntegerField(default=5, null=False, blank=True)
+    image = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.good_content, self.bad_content)
