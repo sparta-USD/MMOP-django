b68480a11ed9b9b11ed590417582484638a60a4b
haeminchoi2 <sjhk02@gmail.com>
2022-12-02 11:38:57 +0900

    Feat: 유저기능을 위한 url추가/수정 #2
    - 회원가입, 로그인을 위해 urls.py 수정 및 추가했습니다.

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+from users import views
+
+
+urlpatterns = [
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('signin/', views.SigninView.as_view(), name='signin'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
