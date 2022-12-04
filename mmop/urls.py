dc08f9349c10f0b1b3c07e91f5b00efb9ccedd11
LeeHyunji <hyunji015@gmail.com>
2022-12-05 00:30:14 +0900

    Feat: static/media 설정 및 django-environ을 이용한 환경변수 관리
    1. static/media 설정
    
    2. django-environ을 이용한 환경변수 관리
        - docker로 배포시 .env파일로 관리를 위해서 미리 셋팅
        - 현재 SECRET_KEY, DEBUG 관리

--- mmop/urls.py
+++ mmop/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# static 파일 경로 설정
+if settings.DEBUG: # DEBUG=True일 때만,
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
