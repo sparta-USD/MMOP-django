fbb537645350f64dfbff0d2e5f1fcab0c4566fcc
LeeHyunji <hyunji015@gmail.com>
2022-12-05 01:02:18 +0900

    Feat: Perfume모델 상세 조회/수정/삭제 API 구현 #12
    1. urls.py 추가
    
    2. GET 'perfume/<id>' 접속 :  Perfume 모델 상세 조회 API
        - PerfumeSerializer를 사용
        - id값으로 데이터를 찾을 수 없으면 404 error 출력
    
    3. PUT 'perfume/<id>' 접속 : Perfume 모델 수정 API
        - PerfumeSerializer를 사용
        - id값으로 데이터를 찾을 수 없으면 404 error 출력
    
    4. Delete 'perfume/<id>' 접속 : Perfume 모델 삭제 API
        - id값으로 데이터를 찾을 수 없으면 404 error 출력
    
    Close #12

--- perfume/urls.py
+++ perfume/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.PerfumeView.as_view(), name="perfume_view"),
+    path("<int:id>/", views.PerfumeDetailView.as_view(), name="perfume_detail_view"),
 ]
--- perfume/views.py
+++ perfume/views.py
@@ -17,3 +17,22 @@ class PerfumeView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PerfumeDetailView(APIView):
+    def get(self, request, id):
+        target_parfume = get_object_or_404(Perfume ,id=id)
+        serializer = PerfumeSerializer(target_parfume)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        target_parfume = get_object_or_404(Perfume ,id=id)
+        serializer = PerfumeSerializer(target_parfume, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        target_parfume = get_object_or_404(Perfume ,id=id)
+        target_parfume.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
