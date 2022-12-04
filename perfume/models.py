35f8aefccc2cd73cb4cbfefd3a6d052df9537556
LeeHyunji <hyunji015@gmail.com>
2022-12-04 23:33:27 +0900

    Feat: Perfume 모델 생성 #6
    1. Perfume 모델 생성
        - origin_id : 'basenotes.com'에서 제공하는 id
        - image : 'basenotes.com'에서 제공하는 이미지주소
        - title : 향수명
        - brand : 향수 브랜드명
        - gender : 사용 성별 F/M/S
        - price : 가격 (ebay 기준)
        - launch_date : 출시년도
        - top_notes : top note 향
        - heart_notes : heart note 향
        - base_notes : base note 향
        - none_notes : top/heart/base가 정해지지 않은 향
    
    2. admin 사이트 추가

--- perfume/models.py
+++ perfume/models.py
@@ -2,6 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Perfume(models.Model):
+    origin_id = models.IntegerField()
+    image = models.CharField(max_length=256,null=True, blank=True)
     title = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100,null=True, blank=True)
+    gender = models.CharField(max_length=5,null=True, blank=True)
+    price = models.FloatField(default=0)
+    launch_date = models.DateField(null=True, blank=True)
+    top_notes = models.TextField(null=True, blank=True)
+    heart_notes = models.TextField(null=True, blank=True)
+    base_notes = models.TextField(null=True, blank=True)
+    none_notes = models.TextField(null=True, blank=True)
 
-    
+    class Meta:
+        db_table = "perfume"
+        verbose_name = '향수'  # 단수형 
+        verbose_name_plural = '향수 제품'  # 복수형
+        ordering = ['-launch_date','brand','title']
+
+    def __str__(self):
+        return self.title
