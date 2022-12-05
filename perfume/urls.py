759184921d593c851e02d87dab6d576db1148974
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-05 14:51:35 +0900

    Feat : review 조회/생성  시 url 변경 #16
    - review 조회/생성 시 url 변경 : <int:perfume_id>/reviews/로 변경
    - 작업내용 2

--- perfume/urls.py
+++ perfume/urls.py
@@ -4,7 +4,6 @@ urlpatterns = [
     path("", views.PerfumeView.as_view(), name="perfume_view"),
     path("<int:id>/", views.PerfumeDetailView.as_view(), name="perfume_detail_view"),
 
-    # <int:perfume_id>/ 추가필요!!
-    path('reviews/', views.ReviewView.as_view(), name='review_view'),
+    path('<int:perfume_id>/reviews/', views.ReviewView.as_view(), name='review_view'),
     path('reviews/<int:review_id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),
 ]
