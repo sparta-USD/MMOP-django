02a0cb5640a8d988e6dfef015f42471c30d2d0a6
LeeHyunji <hyunji015@gmail.com>
2022-12-05 13:20:16 +0900

    HOTFIX: STATIC_ROOT시 django 내부에서 static 접근 불가문제 해결

--- mmop/urls.py
+++ mmop/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 
 # static 파일 경로 설정
 if settings.DEBUG: # DEBUG=True일 때만,
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
