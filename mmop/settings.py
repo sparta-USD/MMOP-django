d6943a666eed9f8b331b14e4b6112cbf00105484
suin <parksuin55@gmail.com>
2022-12-06 11:03:36 +0900

    Feat: 비밀번호 재설정 초기 세팅
    1. settings.py
    	- 이메일 전송 관련 설정 추가
    2. django에서 제공하는 비밀번호 재설정 기본 세팅

--- mmop/settings.py
+++ mmop/settings.py
@@ -181,3 +181,24 @@ SIMPLE_JWT = {
 
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Email 전송 설정
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# 메일을 호스트하는 서버
+EMAIL_HOST = 'smtp.gmail.com'
+
+# gmail과의 통신하는 포트
+EMAIL_PORT = '587'
+
+# 발신할 이메일
+EMAIL_HOST_USER = env("SECRET_EMAIL")
+
+# 발신할 메일의 비밀번호 '구글앱비밀번호'
+EMAIL_HOST_PASSWORD = env("SECRET_PASSWORD")
+
+# TLS 보안 방법
+EMAIL_USE_TLS = True
+
+# 사이트와 관련한 자동응답을 받을 이메일 주소
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
