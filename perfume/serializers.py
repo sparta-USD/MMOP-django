b2c13b9db0e8547e252abfc5d8139e52424cf218
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-07 18:22:01 +0900

    Feat : PerfumeSerializer에서 review 역참조 #36
    - PerfumeSerializer에서 review 역참조
    - PerfumeSerializer를 Review 아래로 이동
    
    Close #36

--- perfume/serializers.py
+++ perfume/serializers.py
@@ -2,24 +2,16 @@ from rest_framework import serializers
 from .models import Perfume
 from perfume.models import Review
 
-# perfume 
-class PerfumeSerializer(serializers.ModelSerializer):
-    class Meta :
-        model = Perfume
-        fields = "__all__"
-
 # review
 class ReviewSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-    perfume = PerfumeSerializer()
-    
     def get_user(self, obj):
         return obj.user.username
     
     class Meta:
         model = Review
         fields = '__all__'
-
+      
 class ReviewCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
@@ -30,3 +22,10 @@ class ReviewUpdateSerializer(serializers.ModelSerializer):
         model = Review
         fields = ('good_content', 'bad_content', 'grade', 'image')
 
+
+# perfume 
+class PerfumeSerializer(serializers.ModelSerializer):
+    perfume_reviews = ReviewSerializer(many=True)
+    class Meta :
+        model = Perfume
+        fields = "__all__"
