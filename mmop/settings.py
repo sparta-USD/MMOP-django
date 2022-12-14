0908bcac60e61d3ae526c4101e22f76648083077
hyunjooooojung <hyunjoo3107@gmail.com>
2022-12-14 16:26:27 +0900

    Feat: settings.py static url 수정
    - users앱에 static폴더 추가 후 settings.py에서 url 수정
    - 작업내용 2

--- mmop/settings.py
+++ mmop/settings.py
@@ -137,11 +137,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "/users/static/"
+
 # Media files
 # https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-MEDIA_ROOT
 MEDIA_URL = '/media/'
