8a85d404a177c7c84b1fdcf682b240588f2251ec
LeeHyunji <hyunji015@gmail.com>
2022-12-24 17:35:47 +0900

    Remove: 배포시 DB migrate 문제로 임시 주석 처리

--- custom_perfume/models.py
+++ custom_perfume/models.py
@@ -28,7 +28,7 @@ class Note(models.Model):
     kor_name = models.CharField(max_length=100)
     image = models.TextField()
     note_category = models.ForeignKey(NoteCategory, on_delete=models.CASCADE, related_name='note01', null=True, blank=True)
-    tag = models.TextField(null=True, blank=True)
+    # tag = models.TextField(null=True, blank=True)
     
 class CustomPerfume(models.Model):
     title = models.CharField(max_length=100)
